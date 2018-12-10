50407a05bab07709bd824e01f27f78a94e722688
Fengyuan <liu.fengyuan@dhs.sg>
2018-12-10 15:41:31 +0800

    Add files via upload

--- NEOPIXEL.ino
+++ NEOPIXEL.ino
@@ -1,12 +1,8 @@
-
-//Sound Level to light show sketch for the
-//autogain microphone Amplifier from Adafruit on pin AO
-//plus neopixel led functions (pin 6) mapped on to different sound levels to give music to light effects
-//
-//*************
-
-//lines below set variables for neopixels
-//hihi
+/*Neopixel Code by group 6
+ *BACKUP: https://github.com/narutoliu/DIP
+ *Last editted 10/12/18 
+ */
+ 
 #include <Adafruit_NeoPixel.h>
 #include <Wire.h>
 #include <Servo.h>
@@ -24,27 +20,23 @@ int sht = 50;//short delay
 int x = 0;
 int soundLevel = 0;
 
-//lines below are for the microphone sampling from Adafruit autogain mic
-
-const int sampleWindow = 200; // Sample window width in mS (50 mS = 20Hz)
-unsigned int sample;
 
 void setup()
 {
   servo.attach(servopin);
-  // Start the I2C Bus as Slave on address 9
+  //start receiving
   Wire.begin(9); 
-  // Attach a function to trigger when something is received.
+  
   Wire.onReceive(receiveEvent);
   
   strip.begin();//initialises neopixels
-  strip.setBrightness(255);// set brightness from 0 to max is 255
+  strip.setBrightness(255);
   strip.show();//clears any previous data in the strip
-  Serial.begin(9600);//set up for checking mic is working
+  Serial.begin(9600);
 }
 
 void receiveEvent(int bytes) {
-  soundLevel = Wire.read();    // read one character from the I2C
+  soundLevel = Wire.read();    //read data from master arduino
   Serial.println("123");
   
 }
@@ -52,7 +44,8 @@ void receiveEvent(int bytes) {
 void loop()
 {
   //open void loop
-  //first run the sound sampling
+
+  //loop for servor
   for(int pos = 0; pos <= 180; pos++){
     servo.write(pos);
     delay(10);
@@ -61,17 +54,16 @@ void loop()
     servo.write(pos);
     delay(20);
   }
+  //end for servor
   
   Serial.print("The volt level is  ");
   Serial.println(soundLevel);//for debugging
 
-//next section is a series of 12 (0-11) 'if' statements which trigger different patterns.
-//it is a combination of a traditional VU style meter fill of the strip
-// combined with randomised animated patterns to keep it interesting
+  //Play patterns depending on soundlevel
   if (soundLevel == 0)
   {
     int level0Color = random(1, 3);
-    //open if 0. When there is silence a rainbow pattern runs
+    //open if 0. When there is silence
     if (level0Color==1){
       Single_theaterChase(strip.Color(200, 0, 0), 80);
       } // Red
@@ -91,7 +83,6 @@ void loop()
 
   if (soundLevel == 1)
   {
-    //open level 1 if statement which contains 4 randomised options
     meteorRain(0xff,0xff,0xff,10, 90, true, 15);
 
   }//end of if sound level 1 options
@@ -122,20 +113,20 @@ void loop()
   else if (soundLevel == 5)
   {
     //open if sound level 5
-        int color;
+    int color;
     color=random(0,2);
     if (color==0){
-      theaterChase(strip.Color(200, 0, 0), 80); // Red
+      theaterChase(strip.Color(200, 0, 0), 80); 
       strip.show();
       delay(lng);
       
     }else if(color==1){
-      theaterChase(strip.Color(0, 200, 0), 80); // Red
+      theaterChase(strip.Color(0, 200, 0), 80); 
       strip.show();
       delay(lng);
       
     }else{
-      theaterChase(strip.Color(0, 0, 200), 80); // Red
+      theaterChase(strip.Color(0, 0, 200), 80); 
       strip.show();
       delay(lng);
     }
@@ -147,17 +138,17 @@ void loop()
     int color;
     color=random(0,2);
     if (color==0){
-      theaterChase(strip.Color(200, 0, 0), 80); // Red
+      theaterChase(strip.Color(200, 0, 0), 80); 
       strip.show();
       delay(lng);
       
     }else if(color==1){
-      theaterChase(strip.Color(0, 200, 0), 80); // Red
+      theaterChase(strip.Color(0, 200, 0), 80);
       strip.show();
       delay(lng);
       
     }else{
-      theaterChase(strip.Color(0, 0, 200), 80); // Red
+      theaterChase(strip.Color(0, 0, 200), 80);
       strip.show();
       delay(lng);
     }
@@ -170,17 +161,17 @@ void loop()
     int color;
     color=random(0,2);
     if (color==0){
-      theaterChase(strip.Color(200, 200, 0), 80); // Red
+      theaterChase(strip.Color(200, 200, 0), 80); 
       strip.show();
       delay(lng);
       
     }else if(color==1){
-      theaterChase(strip.Color(0, 200, 200), 80); // Red
+      theaterChase(strip.Color(0, 200, 200), 80);
       strip.show();
       delay(lng);
       
     }else{
-      theaterChase(strip.Color(200, 0, 200), 80); // Red
+      theaterChase(strip.Color(200, 0, 200), 80);
       strip.show();
       delay(lng);
     }
@@ -193,7 +184,6 @@ void loop()
   else if (soundLevel == 8)
   {
     flash();
-    //close option 9
 
   }//close if sound level 9
 
@@ -225,22 +215,20 @@ void loop()
 
 }//close void loop()
 
+
+
+
 //neopixel functions below
-
-
-
 void flash(){
+  //flash pattern the "BANG BANG BANG"
   for(int i=0;i<10;i++){
     strip.setPixelColor(i, 255, 0, 0);
   }
   for(int i=10;i<20;i++){
     strip.setPixelColor(i, 0, 0, 0);
   }
-    
-      
       strip.show();
       delay(lng);
-
   for(int i=20;i<30;i++){
     strip.setPixelColor(i, 0, 255, 0);
   }
@@ -271,14 +259,12 @@ void flash(){
       strip.show();
       delay(sht);
 
- }//end of if sound level 1 options
-  
-
+ }
 
 
 void colorWipe(uint32_t c, uint8_t wait)
 {
-  //open colowipe
+  //colourwipe
   for(uint16_t i = 0; i < strip.numPixels(); i++)
   {
     strip.setPixelColor(i, c);
@@ -291,7 +277,7 @@ void colorWipe(uint32_t c, uint8_t wait)
 
 void theaterChase(uint32_t c, uint8_t wait)
 {
-  //open theaterchase function
+  //theaterchase
   for (int j = 0; j < 3; j++) //do 3 cycles of chasing
   {
     for (int q = 0; q < 3; q++)
@@ -317,7 +303,7 @@ void theaterChase(uint32_t c, uint8_t wait)
 
 void Single_theaterChase(uint32_t c, uint8_t wait)
 {
-  //open theaterchase function
+  //theaterchase but with only one light
   for (int j = 0; j < 3; j++) //do 3 cycles of chasing
   {
     for (int q = 0; q < 90; q++)
@@ -336,20 +322,20 @@ void Single_theaterChase(uint32_t c, uint8_t wait)
       }
     }
   }
-}//close theater chase function
+}
 
 
 void Split_theaterChase(uint32_t c, uint8_t wait)
 {
-  //open theaterchase function
+  
   for (int j = 0; j < 3; j++) //do 3 cycles of chasing
   {
     for (int q = 0; q < 90; q++)
     {
       for (int i = 0; i < strip.numPixels(); i = i + 90)
       {
-        strip.setPixelColor(i + q, c);  //turn every third pixel on
-        strip.setPixelColor(90-i - q, c);  //turn every third pixel on
+        strip.setPixelColor(i + q, c);  
+        strip.setPixelColor(90-i - q, c);  
       }
       strip.show();
 
@@ -383,7 +369,6 @@ void rainbowCycle(uint8_t wait)  //open rainbow function
     delay(10);
   }
 }//close rainbow function
-//you need to included the code below for the neopixel functions that use the 'wheel' code:
 uint32_t Wheel(byte WheelPos)
 {
   WheelPos = 255 - WheelPos;
@@ -399,64 +384,6 @@ uint32_t Wheel(byte WheelPos)
   WheelPos -= 170;
   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
-
-
-//FIRE
-void Fire(int Cooling, int Sparking, int SpeedDelay) {
-  static byte heat[90];
-  int cooldown;
-  
-  // Step 1.  Cool down every cell a little
-  for( int i = 0; i < 90; i++) {
-    cooldown = random(0, ((Cooling * 10) / 90) + 2);
-    
-    if(cooldown>heat[i]) {
-      heat[i]=0;
-    } else {
-      heat[i]=heat[i]-cooldown;
-    }
-  }
-  
-  // Step 2.  Heat from each cell drifts 'up' and diffuses a little
-  for( int k= 90 - 1; k >= 2; k--) {
-    heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2]) / 3;
-  }
-    
-  // Step 3.  Randomly ignite new 'sparks' near the bottom
-  if( random(255) < Sparking ) {
-    int y = random(7);
-    heat[y] = heat[y] + random(160,255);
-    //heat[y] = random(160,255);
-  }
-
-  // Step 4.  Convert heat to LED colors
-  for( int j = 0; j < 90; j++) {
-    setPixelHeatColor(j, heat[j] );
-  }
-
-  strip.show();
-  delay(SpeedDelay);
-}
-
-void setPixelHeatColor (int Pixel, byte temperature) {
-  // Scale 'heat' down from 0-255 to 0-191
-  byte t192 = round((temperature/255.0)*191);
- 
-  // calculate ramp up from
-  byte heatramp = t192 & 0x3F; // 0..63
-  heatramp <<= 2; // scale up to 0..252
- 
-  // figure out which third of the spectrum we're in:
-  if( t192 > 0x80) {                     // hottest
-    strip.setPixelColor(Pixel, 255, 255, heatramp);
-  } else if( t192 > 0x40 ) {             // middle
-    strip.setPixelColor(Pixel, 255, heatramp, 0);
-  } else {                               // coolest
-    strip.setPixelColor(Pixel, heatramp, 0, 0);
-  }
-} 
-
-
 
 
 //meteor
@@ -480,7 +407,7 @@ void meteorRain(byte red, byte green, byte blue, byte meteorSize, byte meteorTra
   for(int i = 0; i < 80+80; i++) {
     
     
-    // fade brightness all LEDs one step
+    // fade brightness 
     for(int j=0; j<80; j++) {
       if( (!meteorRandomDecay) || (random(10)>5) ) {
         fadeToBlack(j, meteorTrailDecay );        
---
+++ READ_RFIDUID.ino
@@ -0,0 +1,222 @@
+/*RFID READER Code by group 6
+ *BACKUP: https://github.com/narutoliu/DIP
+ *Last editted 10/12/18 
+ */
+
+
+
+#include <MFRC522.h>
+#include <SPI.h>
+#include <SoftwareSerial.h>
+#include "RedMP3.h"
+#include <Wire.h>
+
+#define MP3_RX 7
+#define MP3_TX 8
+MP3 mp3(MP3_RX, MP3_TX);
+
+int8_t index  = 0x01;//the first song in the SD card
+int8_t volume = 0x1b;//Volum ctronol
+
+#define SS_PIN 10
+#define RS_PIN 9
+#define buttonPIN 1
+
+int x = 0;
+
+bool state = 0;
+//char incomingByte;
+
+int lng = 100;  //long delay
+int sht = 50;   //short delay
+
+
+const int sampleWindow = 200; // Sample window width in mS
+unsigned int sample;
+
+MFRC522 rfid(SS_PIN, RS_PIN);
+
+void setup() {
+
+  // Start master
+  Wire.begin();
+  
+  SPI.begin();
+  Serial.begin(9600);
+  rfid.PCD_Init();
+  digitalWrite(SS_PIN, LOW);
+
+  delay(500);//500ms to initialize
+  mp3.playWithVolume(index, volume); //play sound
+  delay(50);//wait before cmmad
+}
+
+void loop() {
+  unsigned long startMillis = millis(); // Start of sample window
+  unsigned int peakToPeak = 0;   // peak-to-peak level
+
+  unsigned int signalMax = 0;
+  unsigned int signalMin = 1024;
+
+  // collect data for 200 mS
+  while (millis() - startMillis < sampleWindow)
+  {
+    
+    sample = analogRead(0);
+    if (sample < 1024)  // toss out out of range data
+    {
+
+      if (sample > signalMax)
+      {
+
+        signalMax = sample;  // save just the max levels
+      }
+      else if (sample < signalMin)
+      {
+        signalMin = sample;  // save just the min levels
+      }
+    }
+  }
+  peakToPeak = signalMax - signalMin;  // max-min = peak-peak amplitude a.k.a deviation
+  double multiplier = 3.3;
+
+  
+  if (peakToPeak < 512 ) {   //control multiplier for level
+    multiplier = 3.3;
+  } else {
+    multiplier = 4.3;
+  }
+
+  
+  double volts = (peakToPeak * multiplier) / 1024; 
+  int sound = (volts * 10);
+
+  int soundLevel = map(sound, 1, 10, 0, 11);  //map sound to 12 levels
+  Serial.print("the volt is:");
+  Serial.println(soundLevel);
+
+
+  Wire.beginTransmission(9); // transmit to device #9
+
+  Wire.write(soundLevel);
+
+
+  Wire.endTransmission();    // stop transmitting
+
+
+
+  
+  //RFID reader starts here
+  int is[10] = {0};
+  int it = 0;
+
+  for (int i = 0; i <= 9; i++) {                //detect new cards
+    is[i] = rfid.PICC_IsNewCardPresent();
+    if (is[i] == 1) {
+      rfid.PICC_ReadCardSerial();
+    }
+    delay(10);
+  }
+
+  for (int i = 0; i <= 9; i++) {
+    it += is[i];
+  }
+  //////////////////////////////////////////////////////////////////////////////////
+  while (it) {                                       //detect new card and play songs
+
+    Serial.println("while it");
+
+    rfid.PICC_ReadCardSerial();
+
+    unsigned long UID_unsigned;
+    UID_unsigned =  rfid.uid.uidByte[0] << 24;
+    UID_unsigned += rfid.uid.uidByte[1] << 16;
+    UID_unsigned += rfid.uid.uidByte[2] << 8;
+    UID_unsigned += rfid.uid.uidByte[3];
+    //Serial.println(UID_unsigned);
+    Serial.println(UID_unsigned);
+    if (UID_unsigned == 4294960036) { //Havanana
+
+      index  = 0x02;
+      //state = 0;
+
+      if (!state) {
+        delay(500);//Requires 500ms to wait for the MP3 module to initialize
+        mp3.playWithVolume(index, volume);
+        delay(50);
+        Serial.print("CARD 1");
+        state = 1;
+      }
+    }
+
+    if (UID_unsigned == 4294954774) {
+      Serial.flush();
+      Serial.println("teststtt");
+      //index  = 0x03;
+      if (!state) {
+        delay(500);//Requires 500ms to wait for the MP3 module to initialize
+        mp3.playWithVolume(0x03, volume);
+        delay(50);
+        Serial.print("CARD 2");
+        state = 1;
+        rfid.PICC_ReadCardSerial();
+      }
+    }
+
+    if (UID_unsigned == 4294958614) {
+      Serial.flush();
+      // index  = 0x04;
+      if (!state) {
+        delay(500);//Requires 500ms to wait for the MP3 module to initialize
+        mp3.playWithVolume(0x04, volume);
+        delay(50);
+        Serial.print("CARD 3");
+        rfid.PICC_ReadCardSerial();
+        state = 1;
+      }
+    }
+
+    if (UID_unsigned == 4294954006) {
+      Serial.flush();
+      //index  = 0x05;
+      if (!state) {
+        delay(500);//Requires 500ms to wait for the MP3 module to initialize
+        mp3.playWithVolume(0x05, volume);
+        delay(50);
+        Serial.print("CARD 4");
+        state = 1;
+        rfid.PICC_ReadCardSerial();
+      }
+    }
+
+    if (UID_unsigned == 4294935460) {//bumblebee
+      Serial.flush();
+      //index  = 0x06;
+      if (!state) {
+        delay(500);//Requires 500ms to wait for the MP3 module to initialize
+        mp3.playWithVolume(0x05, volume);
+        delay(50);
+        Serial.print("CARD 5");
+        rfid.PICC_ReadCardSerial();
+        state = 1;
+      }
+    }
+
+
+    it = 0;
+
+    for (int i = 0; i <= 9; i++) {
+      is[i] = rfid.PICC_IsNewCardPresent();
+      delay(10);
+    }
+    for (int i = 0; i <= 9; i++) {
+      it += is[i];
+    }
+  }
+
+
+
+  state = 0;
+}
+
+//while loop
